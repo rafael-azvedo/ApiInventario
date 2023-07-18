2766086ccb6bec0ff364d1cf82031f013eaa0a1a
Rafael de Azevedo <rafael.deazevedo01@gmail.com>
2023-07-18 14:38:51 -0300

    Criando arquivos

---
+++ src/main/java/com/inventarioApi/demo/InventarioApiApplication.java
@@ -0,0 +1,13 @@
+package com.inventarioApi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventarioApi/demo/InventarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventarioApi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
